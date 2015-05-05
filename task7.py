c4d4aa2dfdc9950f2a79074ac25007524dc35ffc
davidjn2817 <davidjn2817@gmail.com>
2015-05-04 19:42:38 -0500

    Revised Task7 to include functions and better punctuation recognition

--- task7.py
+++ task7.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 import re
 
-line_regex = "C.*"
-sent_regex = ".*\\. C.*"
-sent2_regex = ".*\\."
+def sent_counter(line):
+    sent_regex = '.*[\\.\\?\\"\\!] C.*'
+    sent_result = re.match(sent_regex, line)
+    if sent_result != None:
+        return True
+
+def sent2_counter(line):
+    sent2_regex = '.*[\\.\\?\\"\\!]'
+    sent2_result = re.match(sent2_regex, line)
+    if sent2_result != None:
+        return True
+
+def line_counter(line):
+    line_regex = "C.*"
+    line_result = re.match(line_regex, line)
+    if line_result != None:
+        return True
 
 line_count=0
 sent_count=0
@@ -11,22 +25,13 @@ sent_count=0
 with open("pg2701.txt") as file:
     for line in file:
 
-        line_result = re.match(line_regex, line)
-        sent_result = re.match(sent_regex, line)
-        sent2_result = re.match(sent2_regex, line)
-
-        if line_result != None:
+        if line_counter(line):
             line_count += 1
-            if sent_check == True:
+            if sent2_counter(line):
                 sent_count += 1
 
-        if sent_result != None:
+        if sent_counter(line):
             sent_count += 1
-
-        if sent2_result != None:
-            sent_check = True
-        else:
-            sent_check = False
 
 
     print 'line C: ' + str(line_count)
